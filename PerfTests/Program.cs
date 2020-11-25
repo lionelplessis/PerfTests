878bf8ee41037ba838fd3dd23db138898ef8f0bb
Lionel Plessis <lionelplessis@users.noreply.github.com>
2020-11-25 23:00:13 +0100

    Basic perf test exported to json

---
+++ PerfTests/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace PerfTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var summary = BenchmarkRunner.Run<Md5VsSha256>();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+            }
+        }
+    }
+
+    [JsonExporterAttribute.Full]
+    [JsonExporterAttribute.FullCompressed]
+    public class Md5VsSha256
+    {
+        private SHA256 sha256 = SHA256.Create();
+        private MD5 md5 = MD5.Create();
+        private byte[] data;
+
+        [Params(1000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Md5() => md5.ComputeHash(data);
+    }
+}
