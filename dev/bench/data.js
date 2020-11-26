2fb6833a646393f90bd3702297cad27247cc53b5
github-action-benchmark <github@users.noreply.github.com>
2020-11-26 07:34:48 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 7fcc952a8cb9a24d82458d70fc2ae6c1c36c4aa0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1606347195414,
+  "lastUpdate": 1606376088628,
   "repoUrl": "https://github.com/lionelplessis/PerfTests",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 2265.1086144623932,
             "unit": "ns",
             "range": "± 56.92640747550341"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lionelplessis@users.noreply.github.com",
+            "name": "Lionel Plessis",
+            "username": "lionelplessis"
+          },
+          "committer": {
+            "email": "lionelplessis@users.noreply.github.com",
+            "name": "Lionel Plessis",
+            "username": "lionelplessis"
+          },
+          "distinct": true,
+          "id": "7fcc952a8cb9a24d82458d70fc2ae6c1c36c4aa0",
+          "message": "Artificially slow down impl to test github action",
+          "timestamp": "2020-11-26T08:33:06+01:00",
+          "tree_id": "849f47dc28bc877f247f13da30af0c725bb3018f",
+          "url": "https://github.com/lionelplessis/PerfTests/commit/7fcc952a8cb9a24d82458d70fc2ae6c1c36c4aa0"
+        },
+        "date": 1606376082619,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "PerfTests.Md5VsSha256.Sha256(N: 1000)",
+            "value": 1087957.5835658482,
+            "unit": "ns",
+            "range": "± 1548.1597402999341"
+          },
+          {
+            "name": "PerfTests.Md5VsSha256.Md5(N: 1000)",
+            "value": 2346.33633177621,
+            "unit": "ns",
+            "range": "± 17.9325776444327"
           }
         ]
       }
